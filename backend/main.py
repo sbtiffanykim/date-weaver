a16b179659cac65e2125449b8a38c210facc7af8
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-02-12 20:22:15 +0900

    feat: implement post request

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,27 @@
-from typing import Union
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+from excel_utils import generate_excel
 
-from fastapi import FastAPI
+
+class ExcelGenerationRequest(BaseModel):
+    start_date: str
+    end_date: str
+    repeat_num: int
+
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/generate-file")
+async def create_file(data: ExcelGenerationRequest):
+    try:
+        file_path = generate_excel(data.start_date, data.end_date, data.repeat_num)
+        return FileResponse(
+            file_path,
+            filename="generated_dates.xlsx",
+            media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            status_code=200,
+        )
+    except HTTPException as e:
+        return {"error": e.detail}
