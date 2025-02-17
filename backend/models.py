40f0ef38b66f2ad61036fa4f39a6c8da49df76bd
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-02-17 22:35:07 +0900

    feat: implement a date order validator

--- backend/models.py
+++ backend/models.py
@@ -23,3 +23,17 @@ class ExcelGenerationRequest(BaseModel):
         if not isinstance(value, int) or value <= 0:
             raise ValueError("Repeat number must be greater than 0")
         return value
+
+    # Check if the start_date is earlier than end_date
+    @field_validator("end_date", mode="after")
+    @classmethod
+    def valid_date_order(cls, value, values):
+        start_date = values.data.get("start_date")
+        if start_date is None:
+            raise ValueError("Start date is missing")
+
+        end_date = value
+        if start_date >= end_date:
+            raise ValueError("Start date must be earlier than end date")
+
+        return value
