a62787abf88eb95cb7051e99b074037cb3ace84d
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-02-19 15:29:07 +0900

    fix: improve date formatting and handle cross-year cases

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import xml.etree.ElementTree as ET
+from datetime import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,15 +9,26 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 url = "http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo"
-params = {"serviceKey": SECRET_KEY, "solYear": "2025", "numOfRows": "30"}
 
-response = requests.get(url, params=params)  # response type: xml
-root = ET.fromstring(response.content)
 
-national_holidays = []
+def get_holidays(start_date, end_date):
 
-for day in root.iter("locdate"):
-    national_holidays.append(day.text)
+    years = [start_date.year]
+    # if start_date year != end_date year
+    if start_date.year != end_date.year:
+        years.append(end_date.year)
 
-# print(national_holidays)
-# print(len(national_holidays))
+    print(years)
+
+    national_holidays = []
+    for year in years:
+        params = {"serviceKey": SECRET_KEY, "solYear": year, "numOfRows": "30"}
+        response = requests.get(url, params=params)  # response type: xml
+        # Parsing XML to get holiday date
+        root = ET.fromstring(response.content)
+        for day in root.iter("locdate"):
+            date = day.text
+            formatted_date = datetime.strptime(date, "%Y%m%d").strftime("%Y년 %m월 %d일")
+            national_holidays.append(formatted_date)
+
+    return national_holidays
