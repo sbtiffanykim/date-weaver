d9a30a998a5749e42a91a40d17a89c4c2adbd91e
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-02-19 15:49:51 +0900

    refactor: add error handling and support multi-year holidays

--- backend/urls.py
+++ backend/urls.py
@@ -1,34 +1,35 @@
-import requests
 import os
 import xml.etree.ElementTree as ET
+import requests
+from fastapi import HTTPException
 from datetime import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-
-url = "http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo"
+URL = "http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo"
 
 
 def get_holidays(start_date, end_date):
 
-    years = [start_date.year]
-    # if start_date year != end_date year
-    if start_date.year != end_date.year:
-        years.append(end_date.year)
-
-    print(years)
-
     national_holidays = []
-    for year in years:
+    for year in range(start_date.year, end_date.year + 1):
         params = {"serviceKey": SECRET_KEY, "solYear": year, "numOfRows": "30"}
-        response = requests.get(url, params=params)  # response type: xml
+        response = requests.get(URL, params=params)  # response type: xml
+
+        # Raise an exception if API request fails
+        if response.status_code != 200:
+            raise HTTPException(status_code=500, detail=f"API request failed: {response.status_code}")
+
         # Parsing XML to get holiday date
-        root = ET.fromstring(response.content)
-        for day in root.iter("locdate"):
-            date = day.text
-            formatted_date = datetime.strptime(date, "%Y%m%d").strftime("%Y년 %m월 %d일")
-            national_holidays.append(formatted_date)
+        try:
+            root = ET.fromstring(response.content)
+            dates = [date.text for date in root.findall(".//locdate")]
+            formatted_dates = [f"{date[:4]}년 {date[4:6]}월 {date[6:]}일" for date in dates]
+            national_holidays.extend(formatted_dates)
+
+        except ET.ParseError:
+            raise HTTPException(status_code=500, detail="XML parsing error occurred")
 
     return national_holidays
