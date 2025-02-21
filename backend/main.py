a748ec1fe93a64dac0c4e0d4517e4e61510a0111
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-02-21 23:22:39 +0900

    feat: add form input

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,28 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 from excel_utils import generate_excel
 from models import ExcelGenerationRequest
 
 
 app = FastAPI()
 
+origins = ["http://127.0.0.1:5173", "http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.post("/generate-file")
 async def create_file(data: ExcelGenerationRequest):
     try:
         file_path = generate_excel(data.start_date, data.end_date, data.repeat_num)
+        print(file_path)
         return FileResponse(
             file_path,
             filename="generated_dates.xlsx",
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,72 @@
+import React, { useState } from 'react';
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    startDate: '',
+    endDate: '',
+    repeatNum: 1,
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setUserInput((prev) => ({
+      ...prev,
+      [name]: name === 'repeatNum' ? Number(value) : value,
+    }));
+  };
+
+  const handleClick = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    // event.preventDefault();
+    // console.log(userInput);
+    const response = await fetch('http://localhost:8000/generate-excel', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'appilcation/json',
+      },
+      body: JSON.stringify(userInput),
+    });
+
+    console.log(response);
+
+    if (response.ok) {
+      console.log(response.status);
+    } else {
+      console.log('Failed to create a file', response);
+    }
+  };
 
   return (
     <>
       <div>
+        <h1>Date Weaver</h1>
       </div>
 
+      <form action=''>
+        <input
+          type='date'
+          name='startDate'
+          value={userInput.startDate}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type='date'
+          name='endDate'
+          value={userInput.endDate}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type='number'
+          name='repeatNum'
+          value={userInput.repeatNum}
+          onChange={handleChange}
+          required
+        />
+        <button onClick={handleClick}>Create a file</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
